f50781a2ec6f0d6f63175ee3484b2c5f4b8fe1f5
Amadeus <a.turner3x@gmail.com>
2023-05-11 19:21:36 -0400

    set up server.js file

---
+++ server.js
@@ -0,0 +1,42 @@
+//imports
+
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+
+//Create database connection
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//create express app
+const app =express();
+const PORT = process.env.PORT || 3001;
+
+//introduce cookies
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
