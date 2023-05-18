db6d0c9fea12cd9498568f36401ecf96df17f488
NickLoeffler999 <123021014+NickLoeffler999@users.noreply.github.com>
2023-05-18 19:14:30 -0400

    set up homepage.handlebars and typos

--- render.js
+++ render.js
@@ -1,46 +1,49 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 // Set up the Handlebars engine
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  layoutsDir: __dirname + '/views/layouts'
-}));
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "handlebars");
 
 // Define routes and render templates
 
 //MAIN.HANDLEBARS
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const data = {
-    name: 'John Doe',
+    name: "John Doe",
     age: 30,
     // Add more data properties as needed
   };
 
-  res.render('main', data);
+  res.render("homepage", data);
 });
 
 //LOGIN.HANDLEBARS
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   // No specific data needed for login template
-  res.render('login');
+  res.render("login");
 });
 
 //PROFILE.HANDLEBARS
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   const data = {
-    name: 'John Doe',
-    occupation: 'Developer',
+    name: "John Doe",
+    occupation: "Developer",
     // Add more data properties as needed
   };
 
-  res.render('profile', data);
+  res.render("profile", data);
 });
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+  console.log("Server started on http://localhost:3000");
 });
